e75f2cf20e6eba327535ac744a3667326c01ca90
Carlos Vaca Morales <cvacam7@gmail.com>
2014-10-29 16:56:08 -0600

    Adding login modal Fixing top slider

--- Colca-league/client/view/home/home.js
+++ Colca-league/client/view/home/home.js
@@ -10,41 +10,6 @@ var dataSet = [
     ['Gecko','Firefox 2.0','Win 98+ / OSX.2+','1.8','A'],
     ['Gecko','Firefox 3.0','Win 2k+ / OSX.3+','1.9','A'],
     ['Gecko','Camino 1.0','OSX.2+','1.8','A'],
-    ['Gecko','Camino 1.5','OSX.3+','1.8','A'],
-    ['Gecko','Netscape 7.2','Win 95+ / Mac OS 8.6-9.2','1.7','A'],
-    ['Gecko','Netscape Browser 8','Win 98SE+','1.7','A'],
-    ['Gecko','Netscape Navigator 9','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Mozilla 1.0','Win 95+ / OSX.1+',1,'A'],
-    ['Gecko','Mozilla 1.1','Win 95+ / OSX.1+',1.1,'A'],
-    ['Gecko','Mozilla 1.2','Win 95+ / OSX.1+',1.2,'A'],
-    ['Gecko','Mozilla 1.3','Win 95+ / OSX.1+',1.3,'A'],
-    ['Gecko','Mozilla 1.4','Win 95+ / OSX.1+',1.4,'A'],
-    ['Gecko','Mozilla 1.5','Win 95+ / OSX.1+',1.5,'A'],
-    ['Gecko','Mozilla 1.6','Win 95+ / OSX.1+',1.6,'A'],
-    ['Gecko','Mozilla 1.7','Win 98+ / OSX.1+',1.7,'A'],
-    ['Gecko','Mozilla 1.8','Win 98+ / OSX.1+',1.8,'A'],
-    ['Gecko','Seamonkey 1.1','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Epiphany 2.20','Gnome','1.8','A'],
-    ['Webkit','Safari 1.2','OSX.3','125.5','A'],
-    ['Webkit','Safari 1.3','OSX.3','312.8','A'],
-    ['Webkit','Safari 2.0','OSX.4+','419.3','A'],
-    ['Webkit','Safari 3.0','OSX.4+','522.1','A'],
-    ['Webkit','OmniWeb 5.5','OSX.4+','420','A'],
-    ['Webkit','iPod Touch / iPhone','iPod','420.1','A'],
-    ['Webkit','S60','S60','413','A'],
-    ['Presto','Opera 7.0','Win 95+ / OSX.1+','-','A'],
-    ['Presto','Opera 7.5','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 8.0','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 8.5','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 9.0','Win 95+ / OSX.3+','-','A'],
-    ['Presto','Opera 9.2','Win 88+ / OSX.3+','-','A'],
-    ['Presto','Opera 9.5','Win 88+ / OSX.3+','-','A'],
-    ['Presto','Opera for Wii','Wii','-','A'],
-    ['Presto','Nokia N800','N800','-','A'],
-    ['Presto','Nintendo DS browser','Nintendo DS','8.5','C/A<sup>1</sup>'],
-    ['KHTML','Konqureror 3.1','KDE 3.1','3.1','C'],
-    ['KHTML','Konqureror 3.3','KDE 3.3','3.3','A'],
-    ['KHTML','Konqureror 3.5','KDE 3.5','3.5','A'],
     ['Tasman','Internet Explorer 4.5','Mac OS 8-9','-','X'],
     ['Tasman','Internet Explorer 5.1','Mac OS 7.6-9','1','C'],
     ['Tasman','Internet Explorer 5.2','Mac OS 8-X','1','C'],
@@ -86,9 +51,12 @@ var options = {
 /* Home: Event Handlers and Helpers */
 /*****************************************************************************/
 Template.home.events({
+    'click #mLogin': function (event, tmp) {
+        $('#modalLogin').modal('toggle');
+    }
     /*
      * Example:
-     *  'click .selector': function (e, tmpl) {
+     *  'click .selector': function (event, tmp) {
      *
      *  }
      */
@@ -110,6 +78,7 @@ Template.home.created = function () {
 };
 
 Template.home.rendered = function () {
+
     $('#leagueTable').dataTable( {
         "data": dataSet,
         "lengthMenu": [[25, 50, 100], [25, 50, "TODOS"]],
